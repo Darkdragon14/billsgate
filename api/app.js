b51fad5808b539c534a99b3e9b25107cd8414c84
Darkdragon14 <s.tristan@hotmail.fr>
2021-11-18 23:30:58 +0100

    feat: add jsdoc-swagger

--- api/app.js
+++ api/app.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const app = express();
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+
+const options = {
+  info: {
+    version: '0.0.1',
+    title: 'Bills Gate',
+    license: {
+      name: 'ISC',
+    },
+  },
+  baseDir: __dirname,
+  // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+  filesPattern: './src/*.js',
+  // URL where SwaggerUI will be rendered
+  swaggerUIPath: '/api-docs',
+  // Expose OpenAPI UI
+  exposeSwaggerUI: true,
+  // Expose Open API JSON Docs documentation in `apiDocsPath` path.
+  exposeApiDocs: false,
+  // Open API JSON Docs endpoint.
+  apiDocsPath: '/v3/api-docs',
+  // Set non-required fields as nullable by default
+  notRequiredAsNullable: false,
+  // You can customize your UI options.
+  // you can extend swagger-ui-express config. You can checkout an example of this
+  // in the `example/configuration/swaggerOptions.js`
+  swaggerUiOptions: {},
+};
+
+expressJSDocSwagger(app)(options);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

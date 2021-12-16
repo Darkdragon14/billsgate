8dfec371bb828ddcfe4c8e6799e59593c981a9ec
Darkdragon14 <s.tristan@hotmail.fr>
2021-12-16 23:26:27 +0100

    feat: add bank and company in the router

--- bills-gate-front/src/components/MyRouter.js
+++ bills-gate-front/src/components/MyRouter.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { Route, Routes, } from 'react-router-dom';
 import Invoices from '../pages/invoices/Invoices';
+import Banks from '../pages/banks/Banks';
+import Companies from '../pages/companies/Companies';
 
 export default function MyRouter(props) {
   const { user } = props;
   return (
     <Routes>
       <Route path="/invoices" element={<Invoices user={user} />} />
-      <Route path="/drafts" element={<p> DRAFTS </p>} />
-      <Route path="/trash" element={<p> TRASH </p>} />
+      <Route path="/banks" element={<Banks user={user} />} />
+      <Route path="/companies" element={<Companies user={user} />} />
       <Route path="/spam" element={<p> SPAM </p>} />
     </Routes>
   );

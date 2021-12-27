e89469abf6ddf74cedb29d6c44f0ab915cad8b8a
Darkdragon14 <s.tristan@hotmail.fr>
2021-12-27 14:08:25 +0100

    fix: the persistance of the user

--- bills-gate-front/src/components/MainStructure.js
+++ bills-gate-front/src/components/MainStructure.js
@@ -74,7 +74,7 @@ export default function MainStructure() {
   React.useEffect(() => {
     api('get', '/user/all').then(response => {
       setUsers(response.data);
-      setUser(localStorage.getItem('userid') ? response.data[localStorage.getItem('userid')] : response.data[0]);
+      setUser(localStorage.getItem('userId') ? response.data[localStorage.getItem('userId')] : response.data[0]);
     });
   }, [])
 
